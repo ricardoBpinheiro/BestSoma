bf238bcc25228d17aa06c3845b9af7f8531a3476
ricardoBpinheiro <ricardo.pinheiro@catolicasc.org.br>
2021-08-27 19:31:29 -0300

    Correção de codigo

--- BestSum/frmBestSoma.vb
+++ BestSum/frmBestSoma.vb
@@ -1,7 +1,15 @@
 ﻿'' Testes
 '' 7 [5,3,4,7] = [7]
+'' [3,4]
+'' [7]
+
 '' 8 [2,3,5] = [3, 5]
+
 '' 5 [1,1,1,2,3] = [2, 3]
+'' [1,1,1,1,1]
+'' [1,1,1,2]
+'' [1,1,3]
+'' [3,2]
 
 Public Class frmBestSoma
     Dim dicionarioSomas As New Dictionary(Of Integer, List(Of Integer))
@@ -15,12 +23,25 @@ Public Class frmBestSoma
             End If
         Next
 
+        If dicionarioSomas.ContainsKey(Me.txtNumero.Text) Then
+            Dim combinacao As List(Of Integer) = dicionarioSomas(Me.txtNumero.Text)
+            If ExisteCombinacao(combinacao) Then
+                MsgBox(String.Join(",", combinacao.ToArray()))
+                Return
+            End If
+        End If
+
         Dim lista = BestSum(Me.txtNumero.Text, numeros)
+
+        If Not dicionarioSomas.ContainsKey(Me.txtNumero.Text) Then
+            dicionarioSomas.Add(Me.txtNumero.Text, lista)
+        End If
 
         If lista IsNot Nothing Then
             MsgBox(String.Join(",", lista.ToArray()))
         End If
     End Sub
+
     Private Sub btnTeste_Click(sender As Object, e As EventArgs) Handles btnTeste.Click
         Dim listaNumeros As New List(Of Integer)
         listaNumeros.Add(5)
@@ -32,7 +53,6 @@ Public Class frmBestSoma
 
         Console.WriteLine($"Alvo: 7 | Numeros=[5,3,4,7] | Melhor Soma= {String.Join(",", lista1.ToArray())}")
 
-
         listaNumeros.Clear()
         listaNumeros.Add(2)
         listaNumeros.Add(3)
@@ -41,7 +61,6 @@ Public Class frmBestSoma
         Dim lista2 = BestSum(8, listaNumeros)
 
         Console.WriteLine($"Alvo: 8 | Numeros=[2,3,5] | Melhor Soma= {String.Join(",", lista2.ToArray())}")
-
 
         listaNumeros.Clear()
         listaNumeros.Add(1)
@@ -53,24 +72,15 @@ Public Class frmBestSoma
         Dim lista3 = BestSum(5, listaNumeros)
 
         Console.WriteLine($"Alvo: 5 | Numeros=[1,1,1,2,3] | Melhor Soma= {String.Join(",", lista3.ToArray())}")
-
     End Sub
 
     Private Function BestSum(ByVal numeroAlvo As Integer, listaNumeros As List(Of Integer)) As List(Of Integer)
-
         If numeroAlvo < 0 Then
             Return Nothing
         End If
 
         If numeroAlvo.Equals(0) Then
             Return New List(Of Integer)
-        End If
-
-        If dicionarioSomas.ContainsKey(numeroAlvo) Then
-            Dim combinacao As List(Of Integer) = dicionarioSomas(numeroAlvo)
-            If ExisteCombinacao(combinacao) Then
-                Return dicionarioSomas(numeroAlvo)
-            End If
         End If
 
         Dim menorCombinacao As List(Of Integer) = Nothing
@@ -87,11 +97,6 @@ Public Class frmBestSoma
 
             End If
         Next
-
-        If dicionarioSomas.ContainsKey(numeroAlvo) Then
-            dicionarioSomas.Remove(numeroAlvo)
-        End If
-        dicionarioSomas.Add(numeroAlvo, menorCombinacao)
 
         Return menorCombinacao
     End Function
@@ -112,6 +117,5 @@ Public Class frmBestSoma
         Next
         Return True
     End Function
-
 
 End Class
